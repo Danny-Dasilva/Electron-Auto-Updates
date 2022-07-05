0f2847f36f2e0c854e4a772a658f019f2019e927
Danny-Dasilva <yahchayildasilva@gmail.com>
2022-07-05 13:14:38 -0400

    app reinstall

--- renderer.js
+++ renderer.js
@@ -1,5 +1,5 @@
-const { ipcRenderer } = require('electron');
 
+const { ipcRenderer } = require('electron');
 const select = selector => document.querySelector(selector)
 
 let container = select('#messages')
